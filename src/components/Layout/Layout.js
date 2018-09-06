b235af175dd7fd6a2c49c6add9e0cf13605605cb
Saskia Daller <sad@marketlogicsoftware.com>
2018-09-06 18:09:09 +0200

    added toolbar and logo, overlapping not fixed

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 //wrapper for the core content component we are rendering to the screen
 const layout = ( props ) => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>
       {props.children}
       </main>
